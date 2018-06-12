56f3a09fd607ef827f49cd5b3947214f23286960
mreliopez <elias.selvanovich@gmail.com>
2018-06-12 17:49:40 -0300

    Delete deploy.sh

--- meli-proxy-backend/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REMOTE_IP="200.80.28.114"
-REMOTE_PORT="5000"
-REMOTE_USER="ticpro"
-REMOTE_LOCATION="api/"
-LOCAL_FILES="config package-lock.json package.json server.js"
-
-scp -P$REMOTE_PORT -r $LOCAL_FILES $REMOTE_USER@$REMOTE_IP:$REMOTE_LOCATION
